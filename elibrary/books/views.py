1b91ef46109d41a7f188aa347af3090d4bb3b045
akash <acwankhede@gmail.com>
2015-11-06 13:43:55 +0530

    added modified view

--- elibrary/books/views.py
+++ elibrary/books/views.py
@@ -6,14 +6,25 @@ import json
 from django.core.context_processors import csrf
 from django.contrib import admin	
 from django.contrib.auth.models import User
-
+import os
 
 def books(request):
     return render_to_response('books.html',{'books':Books.objects.all()})
 
 def get_book(request,book_id=None):
-	
-    return render_to_response('home.html',{'book':Books.objects.get(id=book_id)})
+	book=Books.objects.get(id=book_id)
+	flname = str(book.thumbnail).split('.')[0]+'.png'
+	print flname
+	args ={}
+	args['flname']='Media/'+flname
+	args['book_name']='book_name'
+	args['author_name']='author_name'
+	args['description']='description'
+	args['id']='id'
+	args['tag']='BOOK'
+	args[file]='thumbnail'
+	args['book']=book
+	return render_to_response('home.html',args)
 
 def book_data(request):
 	books = Books.objects.all()
@@ -42,9 +53,14 @@ def upload_auth(request):
 	
 	book=Books(book_name=book_name,author_name=author_name,description=description,thumbnail=file,tag=tag,category=category)
 	book.save()
-
+	print str(book.thumbnail)
+	filename = str(book.thumbnail).split('.')[0]
+	print filename
+	cmd = 'gs -sDEVICE=pngalpha -o Media/'+filename+'.png -sDEVICE=pngalpha -dLastPage=1 Media/'+str(book.thumbnail)
+	print cmd
+	os.system(cmd)
 	return HttpResponseRedirect('/books/uploadbook')
-
+	
 def new_book(request):
 	return render_to_response('newbook.html')
 
@@ -63,8 +79,12 @@ def search_titles(request):
 		search_text=request.POST['search_text']
 	else:
 		search_text=''
+	
+	if len(search_text)!=0:
+		books = Books.objects.filter(book_name__contains=search_text)
+	else:
+		books=[]
 
-	books = Books.objects.filter(book_name__contains=search_text)
 	return render_to_response('ajax_search.html',{'books':books})
 
 def save_data(request):
@@ -81,5 +101,4 @@ def save_data(request):
     book.author_name = author_name
     book.description = description
     book.save()
-
     return render_to_response('home.html')
