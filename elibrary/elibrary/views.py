6fa444c8dddca18ac4e506cbeffbe5895df30223
akash <acwankhede@gmail.com>
2015-11-06 14:00:20 +0530

    Modified view in elibrary

--- elibrary/elibrary/views.py
+++ elibrary/elibrary/views.py
@@ -17,7 +17,6 @@ def hello(request):
 
 
 def login(request):
-
     return render_to_response('login.html')
 
 def auth_view(request):
@@ -61,42 +60,55 @@ def register_user(request):
     password = request.POST['password']
     email = request.POST['email']
 
-    user = User(username=username,email=email)
-    user.set_password(password)
-    try:
-        user.save()
-    except IntegrityError:
-        print "User already Exists..."
-        
+    if username!= None:
+        if password != None:
+            if email != None:
+                user = User(username=username,email=email)
+                user.set_password(password)
+                try:                            
+                    user.save()
+                except IntegrityError:
+                    print "User already Exists..."
+
     return HttpResponseRedirect('/accounts/login/')
 
 def register_success(request):
     return render_to_response('register_success.html')
 
-def books_view(request):
+"""def books_view(request):
     books = Books.objects.all()
     args = {}
     args['books'] = books
-    return render_to_response('books_view.html', args)
+    return render_to_response('books_view.html', args)"""
 
 def home(request):
-    books = Books.objects.filter(tag="")
-    args = {}
-    args['tag']=''
-    args['books'] = books
-    return render_to_response('home.html', args)
+    if request.user.is_authenticated():
+        books = Books.objects.filter(tag="")
+        args = {}
+        args['tag']=''
+        args['books'] = books
+        return render_to_response('home.html', args)
+    else:
+        return render_to_response('login.html')
+   
 
 def book_view(request):
+
     books = Books.objects.filter(tag="BOOK")
-    category = Books.objects.filter(category="None")
-    #book_id = Books.objects.filter(id=id)
-    #count = Books.objects.filter(tag="BOOK").count()
-    #print count
+    bookList = []
+    for book in books:
+        flname = str(book.thumbnail).split('.')[0]+'.png'
+        print flname
+        obj = {"flname":'Media/'+flname, "book_name": book.book_name,"author_name":book.author_name,"description":book.description,"thumbnail":str(book.thumbnail),"tag":book.tag,"id":book.id}
+        bookList.append(obj)
+    print bookList
+
     args = {}
     args['tag']='BOOK'
     args['category']='None'
     #args['book_id']='id'
-    args['books'] = books
+    args['books'] = bookList
+    
     print args
     return render_to_response('home.html', args)
 
@@ -105,6 +117,7 @@ def video_view(request):
     #books = Books.objects.filter(category="None")
     args = {}
     args['tag']='VIDEO'
+    args['book_name']='book_name'
     #args['category']='None'
     args['books']=books
     print args
@@ -138,52 +151,88 @@ def comp_view(request):
     tag = Books.objects.filter(tag="BOOK")
     books = Books.objects.filter(category="Computer")
     args = {}
+    bookList = []
+    for book in books:
+        flname = str(book.thumbnail).split('.')[0]+'.png'
+        print flname
+        obj = {"flname":'Media/'+flname, "book_name": book.book_name,"author_name":book.author_name,"description":book.description,"thumbnail":str(book.thumbnail),"tag":book.tag,"id":book.id}
+        bookList.append(obj)
+    print bookList
     args['category']='Computer'
     args['tag']='BOOK'
-    args['books'] = books
+    args['books'] = bookList
+    print args
     return render_to_response('home.html',args)
     
 def mech_view(request):
     books = Books.objects.filter(category="Mechanical",tag="BOOK")
     args = {}
+    bookList = []
+    for book in books:
+        flname = str(book.thumbnail).split('.')[0]+'.png'
+        print flname
+        obj = {"flname":'Media/'+flname, "book_name": book.book_name,"author_name":book.author_name,"description":book.description,"thumbnail":str(book.thumbnail),"tag":book.tag,"id":book.id}
+        bookList.append(obj)
     args['category']='Mechanical'
     args['tag']='BOOK'
-    args['books'] = books
+    args['books'] = bookList
     return render_to_response('home.html',args)
 
 def etc_view(request):
     books = Books.objects.filter(category="E_&_TC",tag="BOOK")
     args = {}
+    bookList = []
+    for book in books:
+        flname = str(book.thumbnail).split('.')[0]+'.png'
+        print flname
+        obj = {"flname":'Media/'+flname, "book_name": book.book_name,"author_name":book.author_name,"description":book.description,"thumbnail":str(book.thumbnail),"tag":book.tag,"id":book.id}
+        bookList.append(obj)
     args['category']='E_&_TC'
     args['tag']='BOOK'
-    args['books'] = books
+    args['books'] = bookList
     return render_to_response('home.html',args)
 
 def civil_view(request):
     books = Books.objects.filter(category="Civil",tag="BOOK")
     args = {}
+    bookList = []
+    for book in books:
+        flname = str(book.thumbnail).split('.')[0]+'.png'
+        print flname
+        obj = {"flname":'Media/'+flname, "book_name": book.book_name,"author_name":book.author_name,"description":book.description,"thumbnail":str(book.thumbnail),"tag":book.tag,"id":book.id}
+        bookList.append(obj)
     args['category']='Civil'
     args['tag']='BOOK'
-    args['books'] = books
+    args['books'] = bookList
     return render_to_response('home.html',args)
 
 def it_view(request):
     books = Books.objects.filter(category="IT",tag="BOOK")
     args = {}
+    bookList = []
+    for book in books:
+        flname = str(book.thumbnail).split('.')[0]+'.png'
+        print flname
+        obj = {"flname":'Media/'+flname, "book_name": book.book_name,"author_name":book.author_name,"description":book.description,"thumbnail":str(book.thumbnail),"tag":book.tag,"id":book.id}
+        bookList.append(obj)
     args['category']='IT'
     args['tag']='BOOK'
-    args['books'] = books
+    args['books'] = bookList
     return render_to_response('home.html',args)
 
 def electrical_view(request):
     books = Books.objects.filter(category="Electrical",tag="BOOK")
     args = {}
+    bookList = []
+    for book in books:
+        flname = str(book.thumbnail).split('.')[0]+'.png'
+        print flname
+        obj = {"flname":'Media/'+flname, "book_name": book.book_name,"author_name":book.author_name,"description":book.description,"thumbnail":str(book.thumbnail),"tag":book.tag,"id":book.id}
+        bookList.append(obj)
     args['category']='Electrical'
     args['tag']='BOOK'
-    args['books'] = books
+    args['books'] = bookList
     return render_to_response('home.html',args)
-
-
 
 def json_view(request):
     books = Books.objects.all()
